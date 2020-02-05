86b0bfb09057f68f757b31c2a13ec5805df50c3f
77ccreed <kiur.jullis@gmail.com>
2020-02-05 12:42:59 +0200

    ..

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,8 +40,8 @@ export const IndexPageTemplate = ({
             className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
             style={{
               boxShadow:
-                'rgb(255, 68, 0, 0.5) 0.5rem 0px 0px, rgb(255, 68, 0, 0.5) -0.5rem 0px 0px',
-              backgroundColor: 'rgb(255, 68, 0, 0.5)',
+                'rgb(255, 68, 0, 0.6) 0.5rem 0px 0px, rgb(255, 68, 0, 0.6) -0.5rem 0px 0px',
+              backgroundColor: 'rgb(255, 68, 0, 0.6)',
               color: 'white',
               lineHeight: '1',
               padding: '0.25em',
