6303b5d2a4a2e291618223416d3d2c4b957d1bd7
77ccreed <kiur.jullis@gmail.com>
2020-02-12 10:34:22 +0200

    tiitel ja desc

--- src/pages/teenused/index.js
+++ src/pages/teenused/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
+import Helmet from 'react-helmet'
 import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
+          <Helmet>
+          <title>Kodulehe tegemine ja veebilehe sisu loomine</title>
+          <meta name="description" content="Sisu loomine, programmeerimine ning arvutialane konsultatsioon. Kõik vajalik ettevõtte veebilehe tegemiseks. Tasuta veebimajutus ja sisuhaldustarkvara." />
+        </Helmet >
         <div
           className="full-width-image-container margin-top-0"
           style={{
