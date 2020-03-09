e01b673d6227d427f2146e8252ad1ce9603e4447
77ccreed <kiur.jullis@gmail.com>
2020-03-09 21:40:21 +0200

    priv pol

--- src/pages/privaatsuspoliitika/index.js
+++ src/pages/privaatsuspoliitika/index.js
@@ -16,19 +16,19 @@ const Privaatsuspoliitika = () => {
               <div className="section</div>">
                 <h1 className="title is-size-3 has-text-weight-bold is-bold-light"> OÜ Kodulehe Tellimine - privaatsuspoliitika</h1>
                 <p className="content">Tellimuse saatmisel kinnitab tellija, et on tutvunud veebilehe tellikoduleht.ee privaatsuse ja andmekaitse tingimustega ning aktsepteerib neid.
-Isikuandmete töötlemine toimub kliendiga sõlmitud lepingu täitmise eesmärgil. Isikuandmete töötlemine toimub juriidilise kohustuse täitmiseks. OÜ Kodulehe Tellimine on isikuandmete vastutav töötleja ja ta töötleb ostja isikuandmeid kooskõlas isikuandmete kaitse seaduses kehtestatud nõuetega. OÜ Kodulehe Tellimine töötleb ostjaga sõlmitud lepingu täitmise eesmärgil järgmiseid isikuandmeid: ettevõtte nimi, kontaktisiku ees- ja perenimi, telefoninumber, aadress, elektronposti aadress, tellimusega seotud andmed.</p>
+Isikuandmete töötlemine toimub kliendiga sõlmitud lepingu täitmise eesmärgil. Isikuandmete töötlemine toimub juriidilise kohustuse täitmiseks. OÜ Kodulehe Tellimine on isikuandmete vastutav töötleja ja ta töötleb ostja isikuandmeid kooskõlas isikuandmete kaitse seaduses kehtestatud nõuetega.</p>
                 <p className="content">Isikuandmete vastutav töötleja on OÜ Kodulehe Tellimine.</p>
-                <p className="content">Andmeid kasutatakse hinnapakkumise tegemiseks ja kontakti võtmiseks. Andmete hoiustamisel lähtume ka raamatupidamise seaduse nõuetest.</p>
+                <p className="content">Andmeid kasutatakse hinnapakkumise tegemiseks, kontakti võtmiseks ja tellimuse täitmiseks. Andmete hoiustamisel lähtume ka raamatupidamise seaduse nõuetest.</p>
                 <p className="content">Juurdepääs isikuandmetele on ettevõtte töötajatel, kes saavad isikuandmetega tutvuda selleks, et lahendada seonduvaid tehnilisi küsimusi ning osutada kliendile teenust. OÜ Kodulehe Tellimine rakendab asjakohaseid turvameetmeid, et kaitsta isikuandmeid juhusliku või ebaseadusliku hävitamise, kaotsimineku, muutmise või loata juurdepääsu ja avalikustamise eest.</p>
-                <p>OÜ Kodulehe Tellimine võib koguda ka isikustamata andmeid, milleks võivad olla andmed tegevuse või aktiivsuse kohta tellikoduleht.ee veebilehel, mida kogutakse statistika ning kliendile parema teenuse pakkumise eesmärgil. Privaatsuspoliitikas käsitleme selliseid andmeid kui isikustamata andmeid.</p>
+                <p>OÜ Kodulehe Tellimine võib koguda ka isikustamata andmeid, milleks võivad olla andmed tegevuse või aktiivsuse kohta tellikoduleht.ee veebilehel, mida kogutakse statistika ning parema teenuse pakkumise eesmärgil. Privaatsuspoliitikas käsitleme selliseid andmeid kui isikustamata andmeid.</p>
                 <p className="content">Isikuandmete kustutamiseks tuleb võtta ühendust e-posti teel. Kustutamistaotlusele vastatakse mitte hiljem kui kuu aja jooksul ning täpsustakse andmete kustutamise aeg. E-posti teel esitatud isikuandmete ülekandmise taotlusele vastatakse hiljemalt kuu aja jooksul.
 OÜ Kodulehe Tellimine kasutab kõiki abinõusid kliendi isikuandmete kaitsmiseks. Tellijal on igal ajal õigus tutvuda oma isikuandmetega ning nõuda nende andmete parandamist, sulgemist või kustutamist, välja arvatud juhul, kui seadus sätestab teisiti. Kui tellija tunneb, et tema õigusi isikuandmete töötlemisel on rikutud, saab ta sellest teada anda pöördudes aadressil kiur.jullis@gmail.com . Vajadusel on õigus pöörduda Andmekaitse Inspektsiooni poole.</p>
 
 
 
                 <h2 className="title is-size-4 has-text-weight-bold is-bold-light">Vaidluste lahendamine</h2>
-                <p className="content">Kui tellijal on veebilehe osas pretensioone, tuleb need saata e-maili aadressile kiur.jullis@gmail.com</p>
-                <p className="content">Kui tellija ja OÜ Kodulehe Tellimine ei suuda lahenda vaidlust kokkuleppe teel, siis on tellijal võimalik pöörduda Tarbijavaidluste komisjoni poole. Tarbijavaidluste komisjoni pädevuses on lahendada ostja ja müüja vahel sõlmitud lepingust tulenevaid vaidlusi.</p>
+                <p className="content">Kui tellijal on OÜ Kodulehe Tellimine osas pretensioone, tuleb need saata e-maili aadressile kiur.jullis@gmail.com</p>
+                <p className="content">Kui tellija ja OÜ Kodulehe Tellimine ei suuda lahenda vaidlust kokkuleppe teel, siis on tellijal võimalik pöörduda Tarbijavaidluste komisjoni poole. Tarbijavaidluste komisjoni pädevuses on lahendada sõlmitud lepingust tulenevaid vaidlusi.</p>
               </div>
             </div>
           </div>
