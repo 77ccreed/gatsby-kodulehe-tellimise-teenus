2c400a7c4889c02896559c5098ba55dbc545fb4e
77ccreed <kiur.jullis@gmail.com>
2020-03-26 08:04:46 +0200

    ..

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Kodulehe tegemine, sisu loomine ja haldus hinnaga 99 € kuus.',
     description:
-      'Telli koduleht koos veebimajutuse. Teeme veebilehest sinu ettevõtte peamise turunduskanali. Pakume konkurentsieelist uuema tehnoloogia kasutamise näol.',
+      'Telli koduleht koos veebimajutusega. Teeme veebilehest sinu ettevõtte peamise turunduskanali. Pakume konkurentsieelist uuema tehnoloogia kasutamise näol.',
     siteUrl: 'https://tellikoduleht.ee'
   },
   plugins: [
