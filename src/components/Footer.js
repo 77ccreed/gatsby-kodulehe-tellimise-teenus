4ba69455df12744506f392b4caa48405102992e3
77ccreed <kiur.jullis@gmail.com>
2020-04-06 16:10:52 +0300

    ettevõtjale => turundus

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,8 +31,8 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/ettevõtjale/">
-                        Ettevõtjale
+                      <Link className="navbar-item" to="/turundus/">
+                        Turundus
                       </Link>
                     </li>
                     <li>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,8 +66,8 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/meist/">
                 Meist
               </Link>
-              <Link className="navbar-item" to="/ettevõtjale/">
-                Ettevõtjale
+              <Link className="navbar-item" to="/turundus/">
+                Turundus
               </Link>
               <Link className="navbar-item" to="/teenused/">
                 Teenused
