f844063760e97de10b6ea7ed00c3b71e1f210ad1
77ccreed <kiur.jullis@gmail.com>
2021-02-27 15:15:23 +0200

    teenuse tingimused

--- src/pages/teenusetingimused/index.js
+++ src/pages/teenusetingimused/index.js
@@ -22,9 +22,7 @@ const Teenusetingimused = () => {
                 <h2 className="title is-size-4 has-text-weight-bold is-bold-light">2. Hinnainfo</h2>
                 <p className="content">2.1. Teenuste hinnad on eurodes ja ei sisalda käibemaksu. Teenuste hinnad on lõplikud ja käibemaksu ei lisandu. Teenuste hinnad kodulehel ja saadavus võivad muutuda ilma etteteatamiseta.</p>
                 <h2 className="title is-size-4 has-text-weight-bold is-bold-light">3. Tellimuse vormistamine</h2>
-                <p className="content">3.1. Teenuse tellimiseks tuleb täita <a href="https://docs.google.com/forms/d/e/1FAIpQLSe9TPVo1_SMyTlpZ6Vo0StZv0H5aoo2-K_P01o9woEdOdUsMA/viewform" target="_blank" rel="noopener noreferrer"
-                  title="Kodulehe tellimise kontaktvorm">küsimustik</a>. Seejärel toimub kohtumine sobivas kohas või interneti vahendusel, millel lepitakse kokku täpsed tingimused.</p>
-                <p className="content">3.2. Tellija on kohustatud enne tellimuse sooritamist kontrollima esitatavate andmete õigsust.</p>
+                <p className="content">3.1. Teenuse tellimiseks saada OÜ Kodulehe Tellimisele kiri. Seejärel toimub kohtumine sobivas kohas või interneti vahendusel, millel lepitakse kokku täpsed tingimused.</p>
                 <h2 className="title is-size-4 has-text-weight-bold is-bold-light">4. Tellimuse eest tasumine</h2>
                 <p className="content">4.1. Teenuse eest tuleb tasuda maksega kokkulepitud summas ja ajal. Teenuse eest tuleb tasuda pangaülekandega.</p>
                 <p className="content">4.2. Tellija ja OÜ Kodulehe Tellimine vahel kokku lepitud hinnad võivad muutuda ainult mõlemapoolsel nõusolekul.</p>
@@ -39,36 +37,13 @@ const Teenusetingimused = () => {
                 <p className="content">6.2. OÜ Kodulehe Tellimine ei võta vahendustasu veebimajutuse teenuse vahendamiselt.</p>
                 <p className="content">6.3. OÜ Kodulehe Tellimine teavitab tellijat viivitamatult kui kodulehe majutamisega kaasneb tasu või muutustest Netlify hinnakirjas tellijat puudutavate teenuste osas, millega kaasneb tasu.</p>
 
-                {/*<h2 className="title is-size-4 has-text-weight-bold is-bold-light">7. Kodulehe tegemine</h2>
-                <p className="content"></p>
-                <p className="content"></p>
-                <p className="content"></p>
+                <h2 className="title is-size-4 has-text-weight-bold is-bold-light">7. Vastutus</h2>
 
-                <h2 className="title is-size-4 has-text-weight-bold is-bold-light">8. Kodulehe sisu kirjutamine</h2>
-                <p className="content"></p>
-                <p className="content"></p>
-                <p className="content"></p>
-
-                <h2 className="title is-size-4 has-text-weight-bold is-bold-light">8. Kodulehe haldus</h2>
-                <p className="content"></p>
-                <p className="content"></p>
-  <p className="content"></p>*/}
-
-
-                <h2 className="title is-size-4 has-text-weight-bold is-bold-light">7. Maksepuhkus tellimuse eest tasumisel</h2>
-                <p className="content">7.1. Kui tellija ei ole OÜ Kodulehe Tellimine poolt osutatava teenusega rahul on tellijal õigus küsida maksepuhkust. Taotluse põhjendatust hindab OÜ Kodulehe Tellimine. Maksepuhkuse andmisest või keeldumisest teavitatakse tellijat 14 päeva jooksul. OÜ Kodulehe Tellimine poolt vastu võetud otsus anda või mitte anda maksepuhkust vaidlusele ei kuulu.</p>
-                <p className="content">7.2. Tellija saab maksepuhkust küsida neljaks kuuks. Kui maksepuhkust antakse ja see aeg möödub, peab tellija jätkama igakuiste maksete tasumist.</p>
-                <p className="content">7.3. Maksepuhkuse ajal jätkab OÜ Kodulehe Tellimine teenuse osutamist kokkulepitud tingimustel ja mahus.</p>
-                <h2 className="title is-size-4 has-text-weight-bold is-bold-light">8. Tellimuse lõpetamine</h2>
-                <p className="content">8.1. Kui tellija ei ole OÜ Kodulehe Tellimine poolt osutatava teenusega rahul on tellijal õigus tellimusest loobuda. Tellimusest loobumisega lõpetab tellija igakuisete maksete sooritamise. Tellimusest loobumisega lisatasusid ei kaasne. Tellimusest loobumiseks tuleb teenuse pakkujat teavitada viivitamatult. Tellimusest loobumisel lõpetab OÜ Kodulehe Tellimine teenuse osutamise. Tellimusest loobudes tellija poolt makstud raha tagastamisele ei kuulu.</p>
-                <p className="content">8.2. OÜ Kodulehe Tellimine võib tellimuse lõpetada ja teenuse osutamise peatada. Sellisel juhul tellija poolt makstud raha tagastamisele ei kuulu. Tellimuse lõpetamisest teatatakse tellijat viivitamata.</p>
-                <p className="content">8.3. OÜ Kodulehe Tellimine lõpetab tellimuse, kui tellija ei tasu makseid kokkulepitud ajal või mahus.</p>
-                <h2 className="title is-size-4 has-text-weight-bold is-bold-light">9. Vastutus ja vääramatu jõud</h2>
-                <p className="content">9.1. OÜ Kodulehe Tellimine ja tellija vastutavad teineteise ees käesolevate tingimuste rikkumisega teisele poolele tekitatud kahju eest Eesti Vabariigis kehtivates õigusaktides sätestatud juhtudel ja ulatuses.</p>
-                <p className="content">9.2. OÜ Kodulehe Tellimine ei vastuta tellijale tekitatud kahjude eest või teenuse osutamise viibimise eest juhul, kui kahju või teenuse osutamise viibimine on tingitud asjaolust, mida OÜ Kodulehe Tellimine ei saanud mõjutada, ei näinud ette ega võinud ette näha (vääramatu jõud).</p>
-                <h2 className="title is-size-4 has-text-weight-bold is-bold-light">10. Vaidluste lahendamine</h2>
-                <p className="content">10.1 Kui tellijal on teenuse osas pretensioone, tuleb need saata e-kirja teel aadressile kiur.jullis@gmail.com</p>
-                <p className="content">10.2. OÜ Kodulehe Tellimine ja tellija vahel tekkinud vaidlused lahendatakse läbirääkimiste teel. Kokkuleppe mittesaavutamisel on tellijal õigus pöörduda oma õiguste kaitseks Tarbijakaitseametisse või kohtusse. Vaidluste ning käesolevates tingimustes mitte välja toodud küsimuste lahendamisel lähtutakse Eesti Vabariigis kehtivatest õigusaktidest.</p>
+                <p className="content">7.1. OÜ Kodulehe Tellimine ja tellija vastutavad teineteise ees käesolevate tingimuste rikkumisega teisele poolele tekitatud kahju eest Eesti Vabariigis kehtivates õigusaktides sätestatud juhtudel ja ulatuses.</p>
+                <p className="content">7.2. OÜ Kodulehe Tellimine ei vastuta tellijale tekitatud kahjude eest või teenuse osutamise viibimise eest juhul, kui kahju või teenuse osutamise viibimine on tingitud asjaolust, mida OÜ Kodulehe Tellimine ei saanud mõjutada, ei näinud ette ega võinud ette näha (vääramatu jõud).</p>
+                <h2 className="title is-size-4 has-text-weight-bold is-bold-light">8. Vaidluste lahendamine</h2>
+                <p className="content">8.1 Kui tellijal on teenuse osas pretensioone, tuleb need saata e-kirja teel aadressile kiur.jullis@gmail.com</p>
+                <p className="content">8.2. OÜ Kodulehe Tellimine ja tellija vahel tekkinud vaidlused lahendatakse läbirääkimiste teel. Kokkuleppe mittesaavutamisel on tellijal õigus pöörduda oma õiguste kaitseks Tarbijakaitseametisse või kohtusse. Vaidluste ning käesolevates tingimustes mitte välja toodud küsimuste lahendamisel lähtutakse Eesti Vabariigis kehtivatest õigusaktidest.</p>
 
               </div>
             </div>
