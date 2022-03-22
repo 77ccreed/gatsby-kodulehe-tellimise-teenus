5b35d9626d610f8a85fa2dc086f18470db66156b
77ccreed <kiur.jullis@gmail.com>
2022-03-22 11:41:57 +0200

    üleliigne pildikaust

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -7,6 +7,10 @@ const PreviewCompatibleImage = ({ imageInfo }) => {
 
   const { alt = "", childImageSharp, image } = imageInfo;
 
+  console.log(imageInfo)
+  console.log(childImageSharp)
+  console.log(image)
+
   if (!!image && !!image.childImageSharp) {
     return (
       <GatsbyImage
